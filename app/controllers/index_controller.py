bd83fd2d03d9f80e9a9ac25dafda17ff95a83da0
maatreef <maatreef33@gmail.com>
2022-11-02 09:11:01 -0300

    Clinics/doctors from the bd in the index

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,15 +1,43 @@
-from app.models.models import Doctors               # Traigo el modelo que realiza la búsqueda en la bd
+from app.models.models import Doctors, Clinics      # Traigo el modelo que realiza la búsqueda en la bd
 
-def index():
+print(Doctors)
 
+def view_doctors():
     list_doctors = []                               # Lo recorro con el for y le agrego los doctores
     for doctor in Doctors.doctors:
-        # print(doctor["name"])                     # Lo vemos la lista de doctores desdel la consola.
+        print(doctor["name"])                       # Lo vemos la lista de doctores desdel la consola.
         doctor_add = doctor["name"]
         list_doctors.append(doctor_add)             # Agregamos la data a la lista
     
     Doctors.doctors.close()                         # Cierro el cursor que se abre al buscar dentro de mongo
-    
     return list_doctors
+    
+list_doctors = view_doctors()
 
-list_doctors = index()
+
+def view_clinics():
+    list_clinics = []                               # Un Array total con otro array dentro
+    for clinic in Clinics.clinics:
+        only_clinic = []                            # Cada clinica es añadida dentro del array general.
+        name_clinicAdd = clinic["name"]
+        only_clinic.append(name_clinicAdd)             
+
+        scheduleAttention_clinicAdd = clinic["scheduleAttention"]
+        only_clinic.append(scheduleAttention_clinicAdd)             
+        
+        phone_clinicAdd = clinic["phone"]
+        only_clinic.append(phone_clinicAdd)             
+        
+        adress_clinicAdd = clinic["address"]["city"]
+        only_clinic.append(adress_clinicAdd)  
+
+        photo_clinicAdd = clinic["photo"]
+        only_clinic.append(photo_clinicAdd)  
+
+        list_clinics.append(only_clinic)             # Agregamos una lista a la lista general
+
+    Clinics.clinics.close()                         # Cierro el cursor que se abre al buscar dentro de mongo
+
+    return list_clinics
+
+list_clinics = view_clinics()
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, url_for, render_template
-from app.controllers.index_controller import list_doctors               # Traigo la lista del controlador.
+from app.controllers.index_controller import list_doctors, list_clinics                                 # Traigo la listas capturada desde el controlador.
 
-global_scope = Blueprint("views", __name__)                             # La carpeta views posee los ficheros estáticos
+global_scope = Blueprint("views", __name__)                                                             # La carpeta views posee los ficheros estáticos
 
 @global_scope.route("/", methods=['GET'])
-def index():
+def view_home():
 
-    return render_template("index.html", list_doctors=list_doctors)     # Se renderiza la plantilla y le paso como argumento la lista 
+    return render_template("index.html", list_doctors=list_doctors, list_clinics=list_clinics)          # Se renderiza la plantilla y le paso como argumento la lista 
